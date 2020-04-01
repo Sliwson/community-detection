6f9d4267e67b4405ec6a7c81d6af5e2cf4e1968f
Ireneusz Stanicki <ireneusz.stanicki@itboom.pl>
2020-04-01 23:00:27 +0200

    Add all seasons json list

---
+++ BasketballReferenceBot/seasonsToOneList.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+var seasons = [];
+
+for (var i = 0; i < 200; i++) {
+  var contents = fs.readFileSync(
+    "./playersSeasons/seasons" + i + ".json",
+    "utf8"
+  );
+
+  var table = JSON.parse(contents);
+  seasons = seasons.concat(table);
+}
+
+fs.writeFile(
+  "./playersSeasons/allSeasons.json",
+  JSON.stringify(seasons),
+  function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  }
+);
