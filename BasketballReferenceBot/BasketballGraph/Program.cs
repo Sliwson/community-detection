d2b7469c110b4e7aeb0df54992cdfdc61fa87db5
Ireneusz Stanicki <ireneusz.stanicki@itboom.pl>
2020-04-03 13:59:26 +0200

    Add graph console app with mutual_friends brute force algorithm

---
+++ BasketballReferenceBot/BasketballGraph/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BasketballGraph
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            GetMutualFriends.ReadCommonRelation();
+        }
+
+
+
+    }
+}
